7e86229f1313b9085ef6de92f5808a3a11899a1c
viniuh <76438208+viniuh@users.noreply.github.com>
2021-09-18 20:37:53 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,10 +4,10 @@ import pytest
 from selenium import webdriver
 
 
-def test_brow(browser):
+def test_add_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.maximize_window()
     basket = browser.find_element_by_css_selector("#add_to_basket_form>.btn")
-    assert basket, "The 'Add_to_basket' button is not presented"
-    time.sleep(2)
+    assert basket.is_displayed(), "The 'Add_to_basket' button is not presented"
+  
